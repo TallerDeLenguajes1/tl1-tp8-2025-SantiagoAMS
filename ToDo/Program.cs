11051899b4b7e077263f4c3135b2bba1ab3c9b18
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-06-03 15:15:06 -0300

    aleatorizacion de tareas

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int NUMERO_TAREAS = 10;
+
+List<Tarea> tareas = new List<Tarea>();
+Random rngesus = new Random();
+
+string[] ts = [
+    "Cocinar la cena",
+    "Darse de baja de metodos",
+    "Reparar las luces",
+    "Reparar las mesas",
+    "Realizar el TP",
+    "Comprar fruta",
+    "Preparar los finales",
+    "Nombre generico de tarea A",
+    "Nombre interesante de tarea B",
+    "Nombre particular de tarea C",
+];
+for (int i = 0; i < NUMERO_TAREAS; i++)
+{
+    tareas.Add(new Tarea(
+        i,
+        ts[i],
+        Math.Min(Math.Max(10, rngesus.Next()), 100)
+    ));
+}
